248d022d147b9f0e5bfd3b9ac1284e2ddc91377d
TanishRadhakrishna <tanish.is22@sahyadri.edu.in>
2025-05-12 14:16:14 +0530

    Vercel analytis

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import styles from '@/styles/Home.module.css';
 import ProjectCard from '@/components/ProjectCard';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import { Analytics } from "@vercel/analytics/next"
 
 const projects = [
   {
